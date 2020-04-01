cf9478e5aeca69eab6e3d4b89f26b56dfe4e6aac
Andres Neff <andres@creo.ch>
2020-04-01 10:24:13 +0200

    Muliplayer support

--- snake.py
+++ snake.py
@@ -16,20 +16,18 @@ coordinates_height = board_height // cell_size
 BLACK = (0, 0, 0)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
 
-class Game:
-    def __init__(self, screen_, cell_size_):
-        self.snake_tiles_x = [coordinates_length // 2]
-        self.snake_tiles_y = [coordinates_height // 2]
-        self.food_x = random.randint(0, coordinates_length - 1)
-        self.food_y = random.randint(0, coordinates_height - 1)
-        self.food_eaten = False
+class Player:
+    def __init__(self, color_):
+        self.snake_tiles_x = [random.randint(coordinates_length // 4, (coordinates_length // 4) * 3)]
+        self.snake_tiles_y = [random.randint(coordinates_length // 4, (coordinates_length // 4) * 3)]
         self.direction = 0      # 0 = up, 1 = right, 2 = down, 3 = left
-        self.screen = screen_
-        self.cell_size = cell_size_
-        self.game_over = False
+        self.color = color_
+        self.game_over_player = False
+        self.food_eaten = False
 
-    def update_game(self):
+    def update_player(self, game):
         if self.direction == 0:
             self.check_cell(self.snake_tiles_x[0], self.snake_tiles_y[0] - 1)
         elif self.direction == 1:
@@ -39,48 +37,107 @@ class Game:
         elif self.direction == 3:
             self.check_cell(self.snake_tiles_x[0] - 1, self.snake_tiles_y[0])
 
-        if self.snake_tiles_x[0] >= coordinates_length or self.snake_tiles_y[0] >= coordinates_height \
-                or self.snake_tiles_x[0] < 0 or self.snake_tiles_y[0] < 0:
-            return False
-
         if not self.food_eaten:
             self.snake_tiles_x.pop(-1)
             self.snake_tiles_y.pop(-1)
 
         self.food_eaten = False
-        found = False
-        while not found:
-            for i in range(len(self.snake_tiles_x)):
-                if self.snake_tiles_x[i] == self.food_x and self.snake_tiles_y[i] == self.food_y:
-                    self.food_eaten = True
-                    self.food_x = random.randint(0, coordinates_length - 1)
-                    self.food_y = random.randint(0, coordinates_height - 1)
-                else:
-                    found = True
+        for i in range(len(self.snake_tiles_x)):
+            if self.snake_tiles_x[i] == game.food_x and self.snake_tiles_y[i] == game.food_y:
+                self.food_eaten = True
 
     def check_cell(self, x, y):
         if x >= coordinates_length or y >= coordinates_height or x < 0 or y < 0:
-            self.game_over = True
+            self.game_over_player = True
 
         for i in range(len(self.snake_tiles_x)):
             if self.snake_tiles_x[i] == x and self.snake_tiles_y[i] == y:
-                self.game_over = True
+                self.game_over_player = True
 
         self.snake_tiles_x.insert(0, x)
         self.snake_tiles_y.insert(0, y)
 
+
+
+
+
+class Game:
+    def __init__(self, screen_):
+        self.food_x = random.randint(0, coordinates_length - 1)
+        self.food_y = random.randint(0, coordinates_height - 1)
+        self.screen = screen_
+        self.game_over = False
+        self.players = []
+
+    def add_player(self, player):
+        self.players.append(player)
+
+    def update_game(self):
+        food_eaten = False
+
+        # update every player movement
+        for player in self.players:
+            player.update_player(self)
+            if player.food_eaten:
+                food_eaten = True
+
+        # check for collisions with other players
+        for player in self.players:
+            other_players = self.players[:]
+            other_players.remove(player)
+            for other_player in other_players:
+                for i in range(len(other_player.snake_tiles_x)):
+                    if player.snake_tiles_x[0] == other_player.snake_tiles_x[i] and \
+                       player.snake_tiles_y[0] == other_player.snake_tiles_y[i]:
+                        player.game_over = True
+
+        # remove players which are game over
+        for player in self.players:
+            if player.game_over_player:
+                self.players.remove(player)
+
+        # update food position if eaten
+        if food_eaten:
+            found_good_position = False
+            while not found_good_position:
+                self.food_x = random.randint(0, coordinates_length - 1)
+                self.food_y = random.randint(0, coordinates_height - 1)
+                found_good_position = True
+
+                for player in self.players:
+                    for i in range(len(player.snake_tiles_x)):
+                        # no collision recognized yet
+                        if found_good_position:
+                            if player.snake_tiles_x[i] == self.food_x and player.snake_tiles_y[i] == self.food_y:
+                                # food collision with snake found
+                                found_good_position = False
+                                break
+                # repeat loop if collision is found
+
     def draw_game(self):
         self.screen.fill(BLACK)
-        for i in range(len(self.snake_tiles_x)):
-            x, y = self.calc_coordinates(self.snake_tiles_x[i], self.snake_tiles_y[i])
-            a_rect = pygame.Rect(x, y, self.cell_size, self.cell_size)
-            pygame.draw.rect(self.screen, GREEN, a_rect)
+        # draw all players
+        for player in self.players:
+            for i in range(len(player.snake_tiles_x)):
+                x, y = self.calc_coordinates(player.snake_tiles_x[i], player.snake_tiles_y[i])
+                a_rect = pygame.Rect(x, y, cell_size, cell_size)
+                pygame.draw.rect(self.screen, player.color, a_rect)
+        # draw food
         x, y = self.calc_coordinates(self.food_x, self.food_y)
-        a_rect = pygame.Rect(x, y, self.cell_size, self.cell_size)
+        a_rect = pygame.Rect(x, y, cell_size, cell_size)
         pygame.draw.rect(self.screen, RED, a_rect)
 
     def calc_coordinates(self, x, y):
-        return x * self.cell_size, y * self.cell_size
+        return x * cell_size, y * cell_size
+
+
+def change_direction(player, new_dir):
+    if len(player.snake_tiles_x) == 1:
+        player.direction = new_dir
+    else:
+        if player.direction != (new_dir+2) % 4:
+            player.direction = new_dir
+
 
 def main():
     pygame.init()
@@ -88,7 +145,12 @@ def main():
     screen = pygame.display.set_mode((board_length, board_height), 0, 32)
     pygame.display.set_caption('Snake')
 
-    my_game = Game(screen, cell_size)
+    my_game = Game(screen)
+    player1 = Player(GREEN)
+    player2 = Player(BLUE)
+
+    my_game.add_player(player1)
+    my_game.add_player(player2)
 
     FPS = 5  # frames per second setting
     fps_clock = pygame.time.Clock()
@@ -99,28 +161,29 @@ def main():
                     (event.type == pygame.KEYUP and event.key == pygame.K_ESCAPE):
                 is_running = False
             elif event.type == KEYDOWN:
-                #WASD
+                # WASD (player 1)
                 if event.key == K_w:
-                    my_game.direction = 0
+                    change_direction(player1, 0)
                 elif event.key == K_d:
-                    my_game.direction = 1
+                    change_direction(player1, 1)
                 elif event.key == K_s:
-                    my_game.direction = 2
+                    change_direction(player1, 2)
                 elif event.key == K_a:
-                    my_game.direction = 3
-                #
+                    change_direction(player1, 3)
+
+                # arrow keys (player 2)
                 elif event.key == K_UP:
-                    my_game.direction = 0
+                    change_direction(player2, 0)
                 elif event.key == K_RIGHT:
-                    my_game.direction = 1
+                    change_direction(player2, 1)
                 elif event.key == K_DOWN:
-                    my_game.direction = 2
+                    change_direction(player2, 2)
                 elif event.key == K_LEFT:
-                    my_game.direction = 3
+                    change_direction(player2, 3)
 
         my_game.update_game()
 
-        if my_game.game_over:
+        if not my_game.players:
             is_running = False
         else:
             my_game.draw_game()
